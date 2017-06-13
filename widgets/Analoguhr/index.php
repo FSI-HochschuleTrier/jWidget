042270d75b5cb5600da3b5922fd5402d84715776
PhilippD <dippelp@hochschule-trier.de>
2017-06-13 23:30:20 +0200

    Add Analog Watch + Allow Zero refresh Rate in Main

--- widgets/Analoguhr/index.php
+++ widgets/Analoguhr/index.php
@@ -1,7 +1,32 @@
-<?php
-/**
+<?php /**
  * Created by PhpStorm.
- * User: Philipp
+ * User: Philipp Dippel
  * Date: 31.05.17
  * Time: 17:44
  */
+
+?>
+
+<div id="mainContainer">
+    <canvas id="mainCanvas" width="2000" height="2000"></canvas>
+</div>
+
+<script type="text/javascript">
+
+    $('document').ready(function ()
+    {
+            $.getScript("widgets/Analoguhr/ClockStylekit.js")
+                .done(function (script, textStatus)
+                {
+                    clock();
+                })
+                .fail(function (jqxhr, settings, exception)
+                {
+                    console.error("Failed to load ClockStylekit");
+                    cosnole.error(exception);
+                });
+
+    });
+
+
+</script>
